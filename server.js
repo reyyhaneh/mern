1e07977d97af7a71ee11dee51225f714e95dbd9a
reyhoondavaee@gmail.com <reyhoondavaee@gmail.com>
2024-03-09 20:35:31 +0330

    logger added

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require ('./middleware/logger')
+
 // initializing the port if it's mentioned in the environment file.
 const PORT = process.env.PORT || 3500
+
+app.use(logger)
+app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
