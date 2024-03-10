0b10bf7844fecaf0c15bf1012bcd1d1e5f49ed82
reyhoondavaee@gmail.com <reyhoondavaee@gmail.com>
2024-03-10 17:47:48 +0330

    Revert "logger added"
    This reverts commit 1e07977d97af7a71ee11dee51225f714e95dbd9a.

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const {logger} = require ('./middleware/logger')
-
 // initializing the port if it's mentioned in the environment file.
 const PORT = process.env.PORT || 3500
-
-app.use(logger)
-app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
