0cc6c7c8f4785708c6dc60b33e938585c1f9701a
reyhoondavaee@gmail.com <reyhoondavaee@gmail.com>
2024-03-10 18:21:53 +0330

    cors aded

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-// initializing the port if it's mentioned in the environment file.
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
 app.use('/', express.static(path.join(__dirname, 'public')))
+
 app.use('/', require('./routes/root'))
 
-// We put this in the end because anything that is noy caught in anyother routes is not found as is a 404.
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
@@ -18,4 +30,7 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
